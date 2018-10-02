7f6167a8e99cdd1efc0b459143ba1856bfc23675
asherchin <royjin@msn.com>
2018-10-02 16:12:22 +0800

    10old files

---
+++ 10oldfiles.py
@@ -0,0 +1,19 @@
+#!/usr/bin/evn python
+#-*- coding: UTF-8 -*-
+
+import os
+import fnmatch
+
+def is_file_match(filename,patterns):
+    for patterns in patterns:
+        if fnmatch.fnmatch(filename,patterns):
+            return True
+    return False
+def find_specific_files(root,patterns=['*'],exclude_dirs=[]):
+    for root,dirnames, filenames in os.walk(root):
+        for filename in filenames:
+            if is_file_match(filename,patterns):
+                yield os.path.join(root,filename)
+                for d in exclude_dirs:
+                    if d in dirnames:
+                        dirnames.remove(d)
